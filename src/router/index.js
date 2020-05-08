bb0444dd82439d009d4873172127ffe9695c480b
fnbellomo <fnbellomo@gmail.com>
2020-05-08 12:56:14 -0300

    Remove default about page

--- src/router/index.js
+++ src/router/index.js
@@ -11,52 +11,46 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/setup',
     name: 'Setup',
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Init.vue'),
     children: [
       {
         path: 'step-1',
         name: 'Step-1',
-        component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Step1.vue')
+        component: () => import(/* webpackChunkName: "setup-1" */ '../views/Setup/Step1.vue')
       },
       {
         path: 'step-2',
         name: 'Step-2',
-        component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Step2.vue')
+        component: () => import(/* webpackChunkName: "setup-2" */ '../views/Setup/Step2.vue')
       },
       {
         path: 'step-3',
         name: 'Step-3',
-        component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Step3.vue')
+        component: () => import(/* webpackChunkName: "setup-3" */ '../views/Setup/Step3.vue')
       },
       {
         path: 'step-4',
         name: 'Step-4',
-        component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Step4.vue')
+        component: () => import(/* webpackChunkName: "setup-4" */ '../views/Setup/Step4.vue')
       },
       {
         path: 'step-5',
         name: 'Step-5',
-        component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Step5.vue')
+        component: () => import(/* webpackChunkName: "setup-5" */ '../views/Setup/Step5.vue')
       },
       {
         path: 'step-6',
         name: 'Step-6',
-        component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Step6.vue')
+        component: () => import(/* webpackChunkName: "setup-6" */ '../views/Setup/Step6.vue')
       },
       {
         path: 'step-7',
         name: 'Step-7',
-        component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Step7.vue')
+        component: () => import(/* webpackChunkName: "setup-7" */ '../views/Setup/Step7.vue')
       }
     ]
   }
