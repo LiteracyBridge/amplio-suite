1f1098e00ed088f5b195aeaf960ce1d5c4d3b0c8
fnbellomo <fnbellomo@gmail.com>
2020-05-08 11:37:11 -0300

    Box of each setup step

---
+++ src/components/SetupBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mt-6 p-6 rounded-lg border border-solid border-gray-300 shadow-lg">
+    <h2 v-if="title !== ''" class="py-4 text-3xl text-blue">{{ title }}</h2>
+
+    <div class="text-xl text-center">
+      <slot />
+    </div>
+
+    <footer class="flex" :class="prev !== '' ? 'justify-between' : 'justify-end'">
+      <router-link v-if="prev !== ''" :to="{ name: prev }">
+        <Button text="PREV" />
+      </router-link>
+
+      <router-link v-if="next !== ''" :to="{ name: next }">
+        <Button text="NEXT" />
+      </router-link>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/Button'
+
+export default {
+  components: {
+    Button
+  },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    prev: {
+      type: String,
+      default: ''
+    },
+    next: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
