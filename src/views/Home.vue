9d3ad1f25994d951fc15ab14757f893c61dca9dd
fnbellomo <fnbellomo@gmail.com>
2020-05-08 10:18:18 -0300

    Add link to /setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="container mx-auto text-center">
+
+    <h1 class="my-20 text-3xl text-blue font-semibold">
+      Welcome to Amplio Jenny!
+    </h1>
+
     <div class="grid grid-cols-3 gap-10">
-      <Box img="/img/plan.png" title="1. Plan and Prepare">
-        <p class="text-justify">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book
-        </p>
-      </Box>
+      <router-link to="/setup">
+        <Box img="/img/plan.png" title="1. Plan and Prepare">
+          <p class="text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it
+            to make a type specimen book
+          </p>
+        </Box>
+      </router-link>
 
       <Box img="/img/learning.png" title="2. Learning Portal">
         <p class="text-justify">
