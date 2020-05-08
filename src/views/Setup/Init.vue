38003244d74514fa96c88d7c8d8712d43490b16e
fnbellomo <fnbellomo@gmail.com>
2020-05-08 11:37:38 -0300

    Component to add child router

---
+++ src/views/Setup/Init.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="container mx-auto mt-20 text-center">
+    <header class="inline-flex">
+      <h1 class="text-2xl">Page 2/7</h1>
+      <progress class="ml-5" max="70" value="20"></progress>
+    </header>
+
+    <router-view />
+  </section>
+</template>
