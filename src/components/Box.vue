066f5f01f9e1eecbb813a99a8976a431b9a0b386
fnbellomo <fnbellomo@gmail.com>
2020-05-07 22:00:40 -0300

    Add box component

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-6 rounded-lg shadow-lg" style="border: 1px solid #e0e0e0;">
+    <img :src="img">
+    <h2 class="py-4 text-xl font-bold">{{ title }}</h2>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String
+    }
+  }
+}
+</script>
