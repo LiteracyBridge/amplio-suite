6b13d0390e02691f5478c589fa29bc0ce775cf6c
fnbellomo <fnbellomo@gmail.com>
2020-05-08 16:39:18 -0300

    Add step text

--- src/views/Setup/Step1.vue
+++ src/views/Setup/Step1.vue
@@ -1,9 +1,20 @@
 <template>
   <Box
     next="Step-2"
-    title="Letʼs start by getting your program details. If your program details change later,
-    you can always update the program specification to match your requirements."
-  />
+    title="Let’s start by getting your program details. If your
+    program details change later, you can always update the program
+    specification to match your requirements."
+  >
+    <p id="programName" class="text-2xl font-semibold">
+      What is your program name?
+    </p>
+    <input
+      type="text"
+      aria-labelledby="programName"
+      placeholder="Enter Program Name"
+      class="mt-2 px-5 py-1 text-base rounded border border-solid border-gray-500"
+    >
+  </Box>
 </template>
 
 <script>
