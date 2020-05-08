9bd3b779837d81a1831471e17ebd88b669cf9977
fnbellomo <fnbellomo@gmail.com>
2020-05-08 13:31:24 -0300

    Add router transition

--- src/views/Setup/Init.vue
+++ src/views/Setup/Init.vue
@@ -5,7 +5,9 @@
       <progress class="ml-5" max="7" :value="step"></progress>
     </header>
 
-    <router-view />
+    <transition :name="transitionName" mode="out-in">
+      <router-view />
+    </transition>
 
     <p class="pt-6">
       Need help? Contact us on <a class="text-blue" href="help@amplio.org">help@amplio.org</a>
@@ -20,14 +22,48 @@ export default {
   computed: mapState([
     'step'
   ]),
-  methods: {
-    ...mapActions([
-      'setStep'
-    ])
+  data () {
+    return {
+      transitionName: 'slide-left'
+    }
   },
   mounted () {
     const step = this.$route.name.split('-')[1]
     this.setStep(step)
+  },
+  beforeRouteUpdate (to, from, next) {
+    const toStep = to.path.split('-')[1]
+    const fromStep = from.path.split('-')[1]
+    this.transitionName = toStep < fromStep ? 'slide-right' : 'slide-left'
+    next()
+  },
+  methods: {
+    ...mapActions([
+      'setStep'
+    ])
   }
 }
 </script>
+
+<style>
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition-duration: 0.250s;
+  transition-property: height, opacity, transform;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate(5em, 0);
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 0;
+  transform: translate(-5em, 0);
+}
+</style>
