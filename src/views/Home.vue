42b753003a7abfc24701cb520655c5a8b5084f0b
fnbellomo <fnbellomo@gmail.com>
2020-05-07 21:58:13 -0300

    Custom colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto text-center">
+    <div class="grid grid-cols-3 gap-10">
+      <Box img="/img/plan.png" title="1. Plan and Prepare">
+        <p class="text-justify">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book
+        </p>
+      </Box>
+
+      <Box img="/img/learning.png" title="2. Learning Portal">
+        <p class="text-justify">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book
+        </p>
+      </Box>
+
+      <Box img="/img/analytics.png" title="3. Analytics Dashboard">
+        <p class="text-justify">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book
+        </p>
+      </Box>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Box from '@/components/Box'
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Box
   }
 }
 </script>
