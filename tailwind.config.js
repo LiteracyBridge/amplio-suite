79a7f4ed6abe00b8d626e1c447362ed4f8a70ba9
fnbellomo <fnbellomo@gmail.com>
2020-05-07 21:59:31 -0300

    Custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        green: '#339966',
+        blue: '#475caa'
+      },
+      minHeight: {
+        '3/4': '75vh'
+      }
+    }
+  }
+}
