b6d821e0072e07bd683235873614fe2735c042c8
fnbellomo <fnbellomo@gmail.com>
2020-05-08 11:36:58 -0300

    Add common button

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="px-5 py-2 text-white bg-green rounded">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
