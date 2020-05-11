6c04d5fd0f3c4a0c4dda8f0b0ebdab52ba47f7e2
fnbellomo <fnbellomo@gmail.com>
2020-05-11 13:08:15 -0300

    Add lang select

--- src/views/Setup/Step7.vue
+++ src/views/Setup/Step7.vue
@@ -1,18 +1,51 @@
 <template>
   <Box
     prev="Step-6"
-    next="Step-7"
-    title="Thank you! We automatically updated the Program Specification based
-    on your responses. Please complete the remaining details."
-  />
+    next="Step-8"
+    title="You are doing great!"
+  >
+    <p class="text-2xl font-semibold">
+      What languages will you develop your content in?
+    </p>
+
+    <v-select
+      v-for="index in amountOfInputs"
+      :key="index"
+      class="w-96 mt-4 mx-auto"
+      placeholder="Choose language"
+      :options="options"
+      @input="(opt) => setLanguages({ opt, index: index - 1 })"
+    />
+
+    <p class="mt-4 font-semibold cursor-pointer" @click="addInput">
+      + Add language
+    </p>
+  </Box>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
+import countryCodes from '@/data/countryCodes'
 import Box from '@/components/SetupBox'
 
 export default {
+  data () {
+    return {
+      amountOfInputs: 2,
+      options: countryCodes
+    }
+  },
   components: {
     Box
+  },
+  methods: {
+    ...mapActions([
+      'setLanguages'
+    ]),
+    addInput () {
+      this.amountOfInputs++
+    }
   }
 }
 </script>
