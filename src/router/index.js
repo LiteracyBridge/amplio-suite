b0b4c09c869811161c69df99f8823ffa45160d69
fnbellomo <fnbellomo@gmail.com>
2020-05-11 15:12:04 -0300

    Redirect /setup to /setup/step-1

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ const routes = [
   {
     path: '/setup',
     name: 'Setup',
+    redirect: { name: 'Step-1' },
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "setup" */ '../views/Setup/Init.vue'),
