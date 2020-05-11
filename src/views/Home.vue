646653c76dafe49e258572ef1639754a47b0762c
fnbellomo <fnbellomo@gmail.com>
2020-05-11 15:00:04 -0300

    Fix text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,28 @@
 <template>
   <div class="container mx-auto text-center">
 
-    <h1 class="my-20 text-3xl text-blue font-semibold">
+    <h1 class="my-20 text-4xl text-blue font-semibold">
       Welcome to Amplio Jenny!
     </h1>
 
     <div class="grid grid-cols-3 gap-10">
       <router-link :to="{ name: 'Step-1' }">
         <Box img="/img/plan.png" title="1. Plan and Prepare">
-          <p class="text-justify">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it
-            to make a type specimen book
+          <p class="text-lg text-justify">
+            Define your program specification and complete installation of required tools
           </p>
         </Box>
       </router-link>
 
       <Box img="/img/learning.png" title="2. Learning Portal">
-        <p class="text-justify">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book
+        <p class="text-lg text-justify">
+          Access learning portal, job aids and community of practice
         </p>
       </Box>
 
       <Box img="/img/analytics.png" title="3. Analytics Dashboard">
-        <p class="text-justify">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book
+        <p class="text-lg text-justify">
+          Monitor user engagement and feedback through the Amplio Dashboard
         </p>
       </Box>
     </div>
