de003d90b0158abbd936f2d58fbc9ca5f0730621
fnbellomo <fnbellomo@gmail.com>
2020-05-12 10:13:22 -0300

    Remove vue-select

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vSelect from 'vue-select'
 
 // Style
-import 'vue-select/dist/vue-select.css'
 import '@/assets/css/tailwind.scss'
 
 Vue.config.productionTip = false
-
-Vue.component('v-select', vSelect)
 
 new Vue({
   router,
