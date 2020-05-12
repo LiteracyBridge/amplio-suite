ffb778c7851c7191f2ebd51f89c08c4ec02ba51c
fnbellomo <fnbellomo@gmail.com>
2020-05-12 15:55:23 -0300

    Compress using gzip and brotli

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg')
@@ -9,5 +11,31 @@ module.exports = {
       .end()
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
+
+    if (process.env.NODE_ENV === 'production') {
+      const gzipPlugin = config.plugin('gzip')
+      gzipPlugin
+        .use(CompressionPlugin, [{
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.js$|\.css$|\.html$/,
+          compressionOptions: { level: 9 },
+          // threshold: 0,
+          // minRatio: 0.1,
+          deleteOriginalAssets: false
+        }])
+
+      const brotliPlugin = config.plugin('brotli')
+      brotliPlugin
+        .use(CompressionPlugin, [{
+          filename: '[path].br[query]',
+          algorithm: 'brotliCompress',
+          test: /\.(js|css|html|svg)$/,
+          compressionOptions: { level: 11 },
+          // threshold: 0,
+          // minRatio: 0.1,
+          deleteOriginalAssets: false
+        }])
+    }
   }
 }
