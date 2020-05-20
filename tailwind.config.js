34f34f3c00e3ff639d5baf94d7eb5bee03a30705
fnbellomo <fnbellomo@gmail.com>
2020-05-20 14:47:53 -0300

    text-3xl is 34px now

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        '3xl': '2.125rem'
+      },
       boxShadow: {
         navbar: '0 1px 2px 0 rgba(60,64,67,0.3),0 2px 6px 2px rgba(60,64,67,0.15)',
         box: '0 1px 2px 0 rgba(60,64,67,.3),0 2px 6px 2px rgba(60,64,67,.15)',
