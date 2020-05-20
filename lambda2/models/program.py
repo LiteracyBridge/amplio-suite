8b2c34e1335ea8f07fec5b9f384bfd8bb41728c6
fnbellomo <fnbellomo@gmail.com>
2020-05-20 12:48:39 -0300

    Column validation is done in the Program class

--- lambda2/models/program.py
+++ lambda2/models/program.py
@@ -1,5 +1,12 @@
-from sqlalchemy.ext.declarative import declarative_base
+from datetime import date
+
 from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.declarative import declarative_base
+
+time_length = ['one_month', 'one_quarter', 'six_months', 'one_year']
+time_period = ['weekly', 'bi_weekly', 'monthly', 'quarterly',
+               'semi_annually', 'annually', 'not_applicable']
 
 Base = declarative_base()
 
@@ -12,3 +19,26 @@ class Program(Base):
     first_deployment = Column(Date, nullable=False)
     feedback_frequency = Column(String(50), nullable=False)
     feedback_frequency2 = Column(String(50), nullable=False)
+
+    @validates('deployment_length')
+    def validate_deployment_length(self, key, deployment_length):
+        if deployment_length not in time_length:
+            raise ValueError("Invalid 'deployment_length' argument")
+        return deployment_length
+
+    @validates('first_deployment')
+    def validate_first_deployment(self, key, first_deployment):
+        assert date.fromisoformat(first_deployment)
+        return first_deployment
+
+    @validates('feedback_frequency')
+    def validate_feedback_frequency(self, key, feedback_frequency):
+        if feedback_frequency not in time_period:
+            raise ValueError("Invalid 'feedback_frequency' argument")
+        return feedback_frequency
+
+    @validates('feedback_frequency2')
+    def validate_feedback_frequency2(self, key, feedback_frequency2):
+        if feedback_frequency2 not in time_period:
+            raise ValueError("Invalid 'feedback_frequency2' argument")
+        return feedback_frequency2
--- lambda2/new_program.py
+++ lambda2/new_program.py
@@ -15,24 +15,6 @@ engine = create_engine(os.getenv('DATABASE_URL'))
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# For validate the selectors
-time_length = {
-    '1 Month': 'one_month',
-    '1 Quarter': 'one_quarter',
-    'Six months': 'six_months',
-    'One year': 'one_year'
-}
-
-time_period = {
-    'Weekly': 'weekly',
-    'Bi-Weekly': 'bi_weekly',
-    'Monthly': 'monthly',
-    'Quarterly': 'quarterly',
-    'Semi-Annually': 'semi_annually',
-    'Annually': 'annually',
-    'Not Applicable': 'not_applicable'
-}
-
 def lambda_handler(event, context):
     """
     Insert new program to the db
@@ -61,26 +43,16 @@ def lambda_handler(event, context):
                 'error': f'{key} must be specified'
             }
 
-    # Validate selectors
-    selectors = [
-        ['deployment_length', time_length],
-        ['feedback_frequency', time_period],
-        ['feedback_frequency2', time_period]
-    ]
+    try:
+        program = Program(**event)
+    except ValueError as err:
+        return {
+            'status': 402,
+            'error': str(err)
+        }
 
-    for (key, opts) in selectors:
-        if event[key] not in opts:
-            return {
-                'status': 402,
-                'error': f'Invalid "{key}"'
-            }
-
-    for (key, opts) in selectors:
-        event[key] = opts[event[key]]
-
-    program = Program(**event)
-    # session.add(program)
-    # session.commit()
+    session.add(program)
+    session.commit()
 
     return {
         'status': 202,
