dd7ad1b730c0d2cd356d788e2380d49aa1f4ed81
Mariano Abel Coca <marianoabelcoca@gmail.com>
2020-05-27 13:45:52 -0300

    Remove unused models

--- lambda/models/program_listening_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, ForeignKey
-
-Base = declarative_base()
-
-class ProgramListeningModel(Base):
-    __tablename__ = "program_listening_models"
-    program_id = Column(Integer, ForeignKey('programs.id'), nullable=False, primary_key=True, index=True)
-    listening_model_id = Column(Integer, ForeignKey('listening_models.id'), nullable=False, primary_key=True, index=True)
--- lambda/models/program_sustainable_development_goal.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, ForeignKey
-
-Base = declarative_base()
-
-class ProgramSustainableDevelopmentGoal(Base):
-    __tablename__ = "program_sustainable_development_goals"
-    program_id = Column(Integer, ForeignKey('programs.id'), nullable=False, primary_key=True, index=True)
-    sustainable_development_goal_id = Column(Integer, ForeignKey('listening_models.id'), nullable=False, primary_key=True, index=True)
