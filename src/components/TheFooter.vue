95079b52ee02d937760b98c1e9b1bd568f1a5f74
fnbellomo <fnbellomo@gmail.com>
2020-05-28 16:11:28 -0300

    Change div tag for footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center py-10 text-white bg-green">
-    <p>© 2018 AMPLIO NETWORK. All rights reserved.</p>
-  </div>
+  <footer class="text-center py-10 text-white bg-green">
+    <p>© 2020 AMPLIO NETWORK. All rights reserved.</p>
+  </footer>
 </template>
