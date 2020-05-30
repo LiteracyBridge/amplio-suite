d3a2a59fa45f92632b6420912616c3371f98ab87
fnbellomo <fnbellomo@gmail.com>
2020-05-30 11:51:59 -0300

    On focus for input

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
       boxShadow: {
         navbar: '0 1px 2px 0 rgba(60,64,67,0.3),0 2px 6px 2px rgba(60,64,67,0.15)',
         box: '0 1px 2px 0 rgba(60,64,67,.3),0 2px 6px 2px rgba(60,64,67,.15)',
-        hover: '0 8px 24px 0px rgba(0,0,0,0.5)'
+        hover: '0 8px 24px 0px rgba(0,0,0,0.5)',
+        outline: '0 0 0 3px rgba(71, 92, 170, 0.7)'
       },
       colors: {
         green: '#289b6a',
