5ac090d4721a6526da60a403291fba2c63966cc4
fnbellomo <fnbellomo@gmail.com>
2020-05-30 11:48:36 -0300

    Add :focus-visible polyfill

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Polyfill :focus-visible
+import 'focus-visible'
+
+// Custom plugins
 import './plugins/fontawesome'
 
 // Style
