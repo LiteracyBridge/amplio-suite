26df66c31d60756d5983c12aadab455b70af6f2c
fnbellomo <fnbellomo@gmail.com>
2020-06-02 10:34:43 -0300

    Generic tag for button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,22 @@
 <template>
-  <button
+  <component
+    :is="tag"
     class="px-5 py-2 text-white bg-green rounded"
+    v-bind="$attrs"
     v-on="$listeners"
   >
     {{ text }}
-  </button>
+  </component>
 </template>
 
 <script>
 export default {
+  inheritAttrs: false,
   props: {
+    tag: {
+      type: String,
+      default: 'button'
+    },
     text: {
       type: String,
       required: true
