7b9c422b1e0994e864e7a4efe510f138237c86f7
fnbellomo <fnbellomo@gmail.com>
2020-06-03 09:28:41 -0300

    Add optional icon

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,18 @@
 <template>
   <component
     :is="tag"
-    class="px-5 py-2 text-white bg-green rounded"
+    :class="color"
+    class="inline-flex items-center px-5 py-2 text-white rounded"
     v-bind="$attrs"
     v-on="$listeners"
   >
-    {{ text }}
+    <font-awesome-icon
+      v-if="iconLeft"
+      :icon="iconLeft"
+      :size="size"
+      :pulse="pulse"
+      class="w-6 h-6" />
+    <span class="ml-2">{{ text }}</span>
   </component>
 </template>
 
@@ -16,6 +23,22 @@ export default {
     tag: {
       type: String,
       default: 'button'
+    },
+    iconLeft: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: 'lg'
+    },
+    pulse: {
+      type: Boolean,
+      default: false
+    },
+    color: {
+      type: String,
+      default: 'bg-green'
     },
     text: {
       type: String,
