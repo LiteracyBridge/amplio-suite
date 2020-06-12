12ef9434f725b018fd0082467a3a2937ab9e7179
fnbellomo <fnbellomo@gmail.com>
2020-06-12 11:05:53 -0300

    CRUD tabs

---
+++ src/views/Program/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <main class="container mx-auto text-center">
+    <div class="my-6 px-4 flex justify-start">
+      <h1 class="text-2xl text-blue">UNICEF Program</h1>
+    </div>
+
+    <nav class="flex">
+      <component
+        v-for="section in sections"
+        :key="section.name"
+        :is="section.disabled ? 'span' : 'router-link'"
+        :to="section.disabled ? '' : `/program/${section.name}`"
+        :class="[
+          $route.path.endsWith(section.name) ? 'bg-green text-white' : 'text-black',
+          section.disabled ? 'opacity-50 cursor-not-allowed' : ''
+        ]"
+        class="py-2 px-4 uppercase text-lg">
+        {{ section.name }}
+      </component>
+    </nav>
+
+     <transition :name="transitionName" mode="out-in">
+      <router-view />
+    </transition>
+
+    <footer class="py-6">
+      Need help? Contact us on <a class="text-blue" href="mailto:support@amplio.org">support@amplio.org</a>
+    </footer>
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sections: [
+        { name: 'general' },
+        { name: 'deployment' },
+        { name: 'content' },
+        { name: 'recipients', disabled: true },
+        { name: 'components', disabled: true },
+        { name: 'values', disabled: true }
+      ],
+
+      transitionName: 'slide-left'
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    const toName = to.path.split('/')[2]
+    const fromName = from.path.split('/')[2]
+    const sections = this.sections.map(section => section.name)
+    this.transitionName = sections.indexOf(toName) < sections.indexOf(fromName) ? 'slide-right' : 'slide-left'
+    next()
+  }
+}
+</script>
