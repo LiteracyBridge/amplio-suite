3e363c2320be0f4ea75a77be7d424bc0c1626809
fnbellomo <fnbellomo@gmail.com>
2020-06-16 14:47:34 -0300

    By default, last wizard step is completed

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
 
   state: {
     actualStep: 1,
-    completedSteps: [0],
+    completedSteps: [0, 7], // First and last step all fill by default
 
     programName: '',
     goals: [],
