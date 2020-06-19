c272e20c830dbf807837254091237520376b7929
fnbellomo <fnbellomo@gmail.com>
2020-06-19 14:59:39 -0300

    Check if the user make change before leave the page

--- src/views/Program/Index.vue
+++ src/views/Program/Index.vue
@@ -26,18 +26,41 @@
     <footer class="py-6">
       Need help? Contact us on <a class="text-blue" href="mailto:support@amplio.org">support@amplio.org</a>
     </footer>
+
+    <v-modal v-model="isModalOpen">
+      <section>
+        <header class="my-4">
+          <h2 class="text-2xl text-bold">Save the change</h2>
+        </header>
+
+        <div class="pt-6 pb-20 text-xl">
+          <p>Save or discard the change before continue.</p>
+        </div>
+
+        <footer class="flex justify-end">
+          <v-button @click="isModalOpen = false" text="Ok" />
+        </footer>
+      </section>
+    </v-modal>
   </main>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 
+import VButton from '@/components/Button'
+import VModal from '@/components/VModal'
+
 export default {
   name: 'Program',
+  components: {
+    VButton,
+    VModal
+  },
   computed: {
-    ...mapState([
-      'programName'
-    ])
+    ...mapState('program', {
+      programName: state => state.general.programName
+    })
   },
   data () {
     return {
@@ -50,7 +73,8 @@ export default {
         { name: 'values', disabled: true }
       ],
 
-      transitionName: 'slide-left'
+      transitionName: 'slide-left',
+      isModalOpen: false
     }
   },
   beforeRouteUpdate (to, from, next) {
@@ -58,7 +82,27 @@ export default {
     const fromName = from.path.split('/')[2]
     const sections = this.sections.map(section => section.name)
     this.transitionName = sections.indexOf(toName) < sections.indexOf(fromName) ? 'slide-right' : 'slide-left'
-    next()
+    const dirty = this.$store.state.program[fromName].dirty
+
+    // Check if the data is save
+    if (dirty) {
+      this.isModalOpen = true
+      next(false)
+    } else {
+      next()
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    const fromName = from.path.split('/')[2]
+    const dirty = this.$store.state.program[fromName].dirty
+
+    // Check if the data is save
+    if (dirty) {
+      this.isModalOpen = true
+      next(false)
+    } else {
+      next()
+    }
   }
 }
 </script>
