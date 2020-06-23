a095813c72d0f088700958de2c1ebdf0a3c49ac7
fnbellomo <fnbellomo@gmail.com>
2020-06-23 10:01:39 -0300

    [WIP] Test get multiple program

--- src/api/programs.api.js
+++ src/api/programs.api.js
@@ -1,9 +1,16 @@
 import httpClient from './httpClient'
+// import store from '@/store'
 
 const END_POINT = '/program'
 
 const postProgram = (program) => httpClient.post(END_POINT, program)
 
+const getPrograms = () => {
+    // const { email } = store.state.account.user
+    return [{ name: 'some program name', id: '' }, { name: 'Program 2', id: '' }]
+}
+
 export {
-    postProgram
+    postProgram,
+    getPrograms
 }
