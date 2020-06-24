80132534c0d25ae33501ddacf820bc73b2b67c16
fnbellomo <fnbellomo@gmail.com>
2020-06-24 12:55:00 -0300

    Return 1 o multiple programs depending on the email

--- src/api/programs.api.js
+++ src/api/programs.api.js
@@ -1,14 +1,18 @@
 import httpClient from './httpClient'
-// import store from '@/store'
+import store from '@/store'
 
 const END_POINT = '/program'
 
 const postProgram = (program) => httpClient.post(END_POINT, program)
 
 const getPrograms = () => {
-    // const { email } = store.state.account.user
-    // return ['Code Name 1', 'Other code name']
-    return ['Code Name 1']
+    const { email } = store.state.account.user
+
+    if (email === 'franco@instedd.org') {
+        return ['Code Name 1']
+    } else {
+        return ['Code Name 1', 'Other code name']
+    }
 }
 
 export {
