53728318c05e7780a395a298c0a563607654f4bd
Mariano Abel Coca <marianoabelcoca@gmail.com>
2020-06-25 14:48:20 -0300

    Add project retrieve function

---
+++ lambda/project_retrieve.py
@@ -0,0 +1,55 @@
+import os
+import sys
+sys.path.append('/var/task/package')
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from utils import get_db_url
+from models.project import Project
+
+from alembic.config import Config
+from alembic import command
+
+from dotenv import load_dotenv
+# Load .env file
+load_dotenv()
+alembic_cfg = Config(os.getenv('ALEMBIC_INI'))
+
+DATABASE_URL = get_db_url()
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def lambda_handler(event, context):
+    try:
+        command.upgrade(alembic_cfg, 'head')
+    except BaseException as err:
+        return {
+            'status': 503,
+            'headers': {'Retry-After': 5},
+            'error': str(err)
+        }
+
+    valid_keys = ['project_id']
+
+    for key in valid_keys:
+        if key not in event:
+            return {
+                'status': 422,
+                'error': f'{key} must be specified'
+            }
+
+    try:
+        project = Project.query.get(project_id)
+        deployments = Deployment.query.filter(Deployment.project == project_id)
+    except ValueError as err:
+        return {
+            'status': 404,
+            'error': str(err)
+        }
+    return {
+        'status': 200,
+        'project': project,
+        'deployments': deployments
+    }
