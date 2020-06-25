57e013098d296b4063152dd01e05238d4be6a740
Mariano Abel Coca <marianoabelcoca@gmail.com>
2020-06-25 15:31:00 -0300

    Add project code to programs

--- lambda/models/program.py
+++ lambda/models/program.py
@@ -26,6 +26,7 @@ class Program(Base):
     __tablename__ = "programs"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), nullable=False)
+    project = Column(String(255), nullable=False)
     sustainable_development_goal = Column(JSON, nullable=False)
     listening_model = Column(JSON, nullable=False)
     amount_deployment = Column(Integer, nullable=False)
