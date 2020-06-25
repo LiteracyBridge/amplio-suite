34abcfbeeab043bf069e9a18e953a685052d78bd
Mariano Abel Coca <marianoabelcoca@gmail.com>
2020-06-25 15:28:47 -0300

    Fix project index and primary key

--- lambda/models/project.py
+++ lambda/models/project.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, UniqueConstraint
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -6,6 +6,9 @@ Base = declarative_base()
 
 class Project(Base):
     __tablename__ = "projects"
+    __table_args__ = (
+        UniqueConstraint('projectcode', name='projects_uniqueness_key'),
+    )
     id = Column(Integer)
     projectcode = Column(String(255), primary_key=True, index=True, nullable=False)
     project = Column(String(255), nullable=False)
