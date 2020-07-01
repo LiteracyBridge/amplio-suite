ccef13d1e520f1e482284c62789a529a3ee3c16e
fnbellomo <fnbellomo@gmail.com>
2020-07-01 15:18:13 -0300

    Load data from lambda

--- src/api/programs.api.js
+++ src/api/programs.api.js
@@ -5,17 +5,19 @@ const END_POINT = '/program'
 
 const postProgram = (program) => httpClient.post(END_POINT, program)
 
-const getPrograms = () => {
-    const { email } = store.state.account.user
+const getPrograms = async () => {
+  const { email } = store.state.account.user
 
-    if (email === 'franco@instedd.org') {
-        return ['Code Name 1']
-    } else {
-        return ['Code Name 1', 'Other code name']
-    }
+  try {
+    const response = await httpClient.post('programs', { email })
+    return Object.keys(response.data.programs)
+  }
+  catch (e) {
+    console.log(e)
+  }
 }
 
 export {
-    postProgram,
-    getPrograms
+  postProgram,
+  getPrograms
 }
