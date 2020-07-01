bc3b868889e4629c47a6627124d31dc2d1a5f0ad
fnbellomo <fnbellomo@gmail.com>
2020-07-01 15:17:59 -0300

    Add timeout

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -4,7 +4,7 @@ const httpClient = axios.create({
     // baseURL: process.env.API_BASE_URL,
     // baseURL: 'http://localhost:9000/api',
     baseURL: 'https://s4ypo6iwud.execute-api.us-west-2.amazonaws.com/staging/',
-    timeout: 5000, // 5 second
+    timeout: 10000
 })
 
 export default httpClient
