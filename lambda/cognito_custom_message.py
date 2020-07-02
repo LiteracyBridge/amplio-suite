d9994dad75edd5d3e3902d4ed0c3d1e9a0d3494a
fnbellomo <fnbellomo@gmail.com>
2020-07-02 14:44:15 -0300

    Send the verification code

--- lambda/cognito_custom_message.py
+++ lambda/cognito_custom_message.py
@@ -1,14 +1,14 @@
 message_new_user = """Thanks for signing up in Amplio-Suite.
-Please click the link below to verify your email address {}"""
+Please use the follow verification code on the login {####}"""
 
-message_forgot_pass = "Please use this reset token to reset your password in Amplio-Suite {}"
+message_forgot_pass = "Please use this reset token to reset your password in Amplio-Suite {####}"
 
 def lambda_handler(event, context):
 	if event['triggerSource'] == 'CustomMessage_SignUp':
 		event['response']['emailSubject'] = "Welcome to Amplio-Suite"
-		event['response']['emailMessage'] = message_new_user.format(event['request']['codeParameter'])
+		event['response']['emailMessage'] = message_new_user
 	elif event['triggerSource'] == 'CustomMessage_ForgotPassword':
 		event['response']['emailSubject'] = "Forgot password for Amplio-Suite"
-		event['response']['emailMessage'] = message_forgot_pass.format(event['request']['codeParameter'])
+		event['response']['emailMessage'] = message_forgot_pass
 
 	return event
