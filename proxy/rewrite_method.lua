6ca9c4ba255209050e3d2a9e6d3ed4e65ecb560f
fnbellomo <fnbellomo@gmail.com>
2020-07-14 22:12:26 -0300

    Rewrite the http method and data

---
+++ proxy/rewrite_method.lua
@@ -0,0 +1,30 @@
+-- Remove query parameters from request
+local clear_request = string.gsub(ngx.var.request, "?.* ", " ")
+ngx.var.original_request = clear_request
+
+local original_method = ngx.var.request_method
+
+-- Rewrite the query parameters to body for the post
+if original_method ==  "GET" then
+  local body = "{"
+  local args, err = ngx.req.get_uri_args()
+  ngx.var.args = ""
+
+  for key, val in pairs(args) do
+    body = body .. '"' .. key .. '": "' .. val .. '" ,'
+  end
+
+  if body:sub(-1, -1) == "," then
+    body = body:sub(1, -2)
+  end
+
+  body = body .. "}"
+
+  ngx.req.read_body()
+  ngx.req.set_body_data(body)
+end
+
+-- Update the http method
+if original_method != "POST" then
+  ngx.req.set_method(ngx.HTTP_POST)
+end
