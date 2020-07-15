d3f2488832e6de32aa867dcd93b8183d653a3acf
fnbellomo <fnbellomo@gmail.com>
2020-07-14 22:13:50 -0300

    get backend url for the env

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    // baseURL: process.env.API_BASE_URL,
-    // baseURL: 'http://localhost:9000/api',
-    baseURL: 'https://s4ypo6iwud.execute-api.us-west-2.amazonaws.com/staging/',
+    baseURL: process.env.VUE_APP_BACK_BASE_URL,
     timeout: 10000
 })
 
