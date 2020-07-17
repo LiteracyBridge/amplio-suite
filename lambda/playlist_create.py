e125861eaaf1587d810e55e16b4d404b62d66956
fnbellomo <fnbellomo@gmail.com>
2020-07-17 17:18:29 -0300

    Lambda function to create playlist and message

---
+++ lambda/playlist_create.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('/var/task/package')
+
+from utils import create_db_session
+from decorators import migration, validate_keys
+from models.content import Content
+
+session = create_db_session()
+
+@migration
+@validate_keys(['program_code', 'deployment_id'])
+def lambda_handler(event, context):
+    try:
+        content = session.query(Content) \
+            .filter(
+                Content.program_code == event['program_code'],
+                Content.deployment_id == event['deployment_id']
+            ) \
+            .first()
+
+        content.add_empty_playlist()
+
+        session.query(Content) \
+            .filter(
+                Content.program_code == event['program_code'],
+                Content.deployment_id == event['deployment_id']
+            ) \
+            .update(content.to_dict())
+
+        session.commit()
+
+        return {
+            'status': 202,
+            'message': 'successfully create a new playlist'
+        }
+    except BaseException as err:
+        return {
+            'status': 422,
+            'error': str(err)
+        }
