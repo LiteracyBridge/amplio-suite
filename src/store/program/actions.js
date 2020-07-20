308bcbb7b9ded5386eed1606e722839b0595c7b2
fnbellomo <fnbellomo@gmail.com>
2020-07-20 12:32:51 -0300

    When get a program, set the program on programData store

--- src/store/program/actions.js
+++ src/store/program/actions.js
@@ -5,10 +5,12 @@ const fetchProgram = async ({ commit }, programCode) => {
   try {
     let program = await getProgram(programCode)
     commit('getProgramSuccess', program)
+    commit('programData/setProgram', program, { root: true })
   } catch (error) {
     commit('getProgramError', error)
   }
 }
+
 
 export default {
   fetchProgram
