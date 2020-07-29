045e61ac14103c0b21777f33aa37cc09e4b39db7
fnbellomo <fnbellomo@gmail.com>
2020-07-29 12:24:45 -0300

    Lambda function to generate a content's CSV of some program

---
+++ lambda/generate_csv.py
@@ -0,0 +1,40 @@
+import io
+import csv
+import sys
+sys.path.append('/var/task/package')
+
+from utils import create_db_session
+from decorators import migration, validate_keys
+from models.content import Content
+
+header = ['deployment_num',	'playlist_title', 'message_title', 'key_points',
+    'languagecode',	'variant', 'default_category', 'sdg_goals',	'sdg_targets']
+
+session = create_db_session()
+
+@migration
+@validate_keys(['program_code'])
+def lambda_handler(event, context):
+    output =  io.StringIO()
+    writer = csv.DictWriter(output, fieldnames=header)
+    writer.writeheader()
+
+    contents = session.query(Content) \
+        .filter(Content.program_code == event['program_code']) \
+        .all()
+
+    for content in contents:
+        for playlist in content.content:
+            rows = [{
+                'playlist_title': playlist['title'], 'message_title': message['title'],
+                'key_points': message['key_point'], 'languagecode': message['language'],
+                'variant': message['variant'], 'default_category': message['default_category'],
+                'sdg_goals': message['sdg_goal'], 'sdg_targets': message['sdg_target']
+            } for message in playlist['messages']]
+
+            writer.writerows(rows)
+
+    return {
+        'status': 200,
+        'csv': output.getvalue()
+    }
