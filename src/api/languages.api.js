1f57a3fbd63a07b1ab9082d3611471aa0b20ffce
Matias Garcia Isaia <mgarcia@manas.tech>
2020-08-03 15:50:04 -0300

    Let languages API throw upon HTTP errors
    The corresponding action should take care of catching & handling the
    exception (usually commiting an error, maybe retrying).
    
    See AMP-19

--- src/api/languages.api.js
+++ src/api/languages.api.js
@@ -1,13 +1,8 @@
 import httpClient from './httpClient'
 
 const getLanguages = async () => {
-  try {
-    const response = await httpClient.get('/languages')
-    return response.data.languages
-  }
-  catch (e) {
-    console.log(e)
-  }
+  const response = await httpClient.get('/languages')
+  return response.data.languages
 }
 
 export {
