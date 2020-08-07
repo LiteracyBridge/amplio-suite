db1103e3d2cf1509882b455de1654912462ffe8f
fnbellomo <fnbellomo@gmail.com>
2020-08-07 11:50:11 -0300

    Extract deployment select to a component
    This new component is shared between content and recipients tabs

--- src/views/Program/Content.vue
+++ src/views/Program/Content.vue
@@ -5,86 +5,49 @@
     title="content"
     help="You can modify your content details on this page. All fields with an asterisk are required. The optional fields are recommended for reporting."
   >
-    <div class="-mx-6">
-      <h3 class="px-6 py-4 bg-gray-400 text-xl text-left border-2 border-gray-600">
-        <select
-          ref="selectDeplo"
-          @change="(event) => changeDeployment(event.target.value)"
-        >
-          <option
-            v-for="item in deployments"
-            :key="item.deployment"
-            :value="item.deploymentname"
-          >
-            Deployment {{ item.deployment }}
-          </option>
-        </select>
-      </h3>
-      <div
-        class="grid transition-all duration-700"
-        style="grid-template-columns: 1.3fr 5fr;"
-      >
-        <playlist-menu />
+    <select-deploymet :dirty="dirty" :on-change="(deployment) => fetchContent(deployment)" />
 
-        <div class="text-left">
-          <playlist-header />
+    <div class="grid" style="grid-template-columns: 1.3fr 5fr;">
+      <playlist-menu />
 
-          <div class="p-4 text-xl bg-gray-400">
-            <p>Messages</p>
-          </div>
+      <div class="text-left">
+        <playlist-header />
 
-          <playlist-messages />
+        <div class="p-4 text-xl bg-gray-400">
+          <p>Messages</p>
         </div>
+
+        <playlist-messages />
       </div>
     </div>
-
-    <!-- For modal components -->
-    <portal to="modalBody" v-if="isModalOpen">
-      <p>Save or discard the change before continue.</p>
-    </portal>
-
-    <portal to="modalFooter" v-if="isModalOpen">
-      <footer class="flex flex-row-reverse justify-between">
-        <v-button @click="handleCloseModal" text="Ok" />
-      </footer>
-    </portal>
   </box>
 </template>
 
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 import { eventBus } from '@/eventBus'
 
 import Box from '@/components/ProgramBox'
-import VButton from '@/components/Button'
+import SelectDeploymet from '@/components/ProgramSelectDeploymet'
 import PlaylistMenu from '@/components/ContentPlaylistMenu'
 import PlaylistHeader from '@/components/ContentPlaylistHeader'
 import PlaylistMessages from '@/components/ContentPlaylistMessages'
 
 export default {
   computed: {
-    ...mapState('deployments', {
-      deployments: state => state.items
-    }),
     ...mapState('content', [
       'status',
       'dirty',
     ]),
-    ...mapGetters('uiSettings', [
-      'selectedDeployment'
-    ])
   },
   components: {
     Box,
-    VButton,
+    SelectDeploymet,
     PlaylistMenu,
     PlaylistHeader,
     PlaylistMessages,
   },
-  data: () => ({
-    isModalOpen: false
-  }),
   mounted (){
     this.fetchCategories()
 
@@ -100,13 +63,6 @@ export default {
     eventBus.$off('discard-crud-data')
   },
   methods: {
-    ...mapActions('ui', [
-      'setModal',
-      'closeModal'
-    ]),
-    ...mapActions('uiSettings', [
-      'setDeploymentIndex'
-    ]),
     ...mapActions('content', [
       'fetchContent',
       'updateContent',
@@ -114,28 +70,6 @@ export default {
     ...mapActions('categories', [
       'fetchCategories'
     ]),
-    handleOpenModal () {
-      this.isModalOpen = true
-      this.setModal('Save or discard the change')
-    },
-    handleCloseModal () {
-      this.isModalOpen = false
-      this.closeModal()
-    },
-    changeDeployment(deploymentName) {
-      if (this.dirty) {
-        this.handleOpenModal()
-        this.$refs.selectDeplo.value = this.selectedDeployment.deploymentname
-        return
-      }
-
-      const index = this.deployments
-        .map(item => item.deploymentname)
-        .indexOf(deploymentName)
-
-      this.fetchContent(deploymentName)
-      this.setDeploymentIndex(index)
-    }
   }
 }
 </script>
