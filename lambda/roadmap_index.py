f8c5ab40b07bb4a5009b997aa409969c1ee56ded
fnbellomo <fnbellomo@gmail.com>
2020-08-19 10:53:26 -0300

    For #106 Roadmap model, migration and index/update lambda

---
+++ lambda/roadmap_index.py
@@ -0,0 +1,25 @@
+from utils import create_db_session
+from decorators import migration, validate_keys
+from models.roadmap import Roadmap
+
+session = create_db_session()
+
+@migration
+@validate_keys(['program_code'])
+def lambda_handler(event, context):
+    roadmap = session.query(Roadmap) \
+        .filter(
+            Roadmap.program_code == event['program_code']
+        ) \
+        .first()
+
+    if roadmap:
+        completed = roadmap.to_dict()['completed']
+    else:
+        completed = []
+
+    return {
+        'status': 200,
+        'program_code': event['program_code'],
+        'roadmap': completed
+    }
