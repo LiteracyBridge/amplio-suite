420ff96d26e8f58256b9c0697cfb76d901aebeb4
fnbellomo <fnbellomo@gmail.com>
2020-08-20 16:47:44 -0300

    For #160 Add recipients table

--- src/views/Program/Recipients.vue
+++ src/views/Program/Recipients.vue
@@ -5,31 +5,63 @@
     title="recipients"
     help="You can modify your recipients details on this page. All fields with an asterisk are required. The optional fields are recommended for reporting."
   >
-    <div class="grid" style="grid-template-columns: 1fr 4fr;">
-      <program-side-menu
-        name="recipient"
-        v-model="recipients"
-        :on-select="(index) => { this.selectedRecipientIndex = index }"
-        :on-add="addRecipient"
-        :on-remove="removeRecipient"
-        :selected-index="selectedRecipientIndex"
-      />
+    <table class="w-full table-auto">
+      <thead>
+        <tr>
+          <th
+            v-for="col in cols"
+            :key="col.key"
+            class="px-4 py-2 text-green border-b"
+          >
+            {{ col.label }}
+          </th>
+          <th class="px-4 py-2 text-green border-b">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(recipient, index) in recipients"
+          :key="recipient.title"
+          :class="index % 2 === 0 ? 'hover:bg-gray-200' : 'bg-gray-200 hover:bg-gray-300'"
+        >
+          <td
+            v-for="col in cols"
+            :key="`${recipient.title}-${col.key}`"
+            class="px-4 py-2 border-b"
+          >
+            {{ recipient[col.key] }}
+          </td>
+          <td class="px-4 py-2 border-b">
+            <button
+              class="mr-3 icon-zoom-xl"
+              @click="onClickEdit(index)"
+            >
+              <font-awesome-icon icon="edit" />
+            </button>
+            <button
+              class="text-red-500 icon-zoom-xl"
+            >
+              <font-awesome-icon icon="times" />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-      <div class="text-left">
-        <program-recipients-form
-          v-if="recipients.length > 0"
-          :recipientIndex="selectedRecipientIndex"
-          :recipient="recipient"
-        />
-      </div>
-    </div>
+    <portal to="modalBody" v-if="showModal.edit">
+      <program-recipients-form
+        v-if="recipients.length > 0"
+        :recipientIndex="selectedRecipientIndex"
+        :recipient="recipient"
+      />
+    </portal>
 
     <!-- For modal components -->
     <portal to="modalBody" v-if="showModal">
       <p>Please complete all of the mandatory fields.</p>
     </portal>
 
-    <portal to="modalFooter" v-if="showModal">
+    <portal to="modalFooter" v-if="showModal.mandatory">
       <footer class="flex flex-row-reverse justify-between">
         <v-button
           @click="onCloseModal"
@@ -49,8 +81,38 @@ import { eventBus } from '@/eventBus'
 
 import VButton from '@/components/Button'
 import Box from '@/components/ProgramBox'
-import ProgramSideMenu from '@/components/ProgramSideMenu'
 import ProgramRecipientsForm from '@/components/ProgramRecipientsForm'
+
+const cols = [
+  {
+    label: 'Region',
+    key: 'region'
+  },
+  {
+    label: 'District',
+    key: 'district'
+  },
+  {
+    label: 'Community',
+    key: 'community'
+  },
+  {
+    label: 'Group',
+    key: 'groupName'
+  },
+  {
+    label: 'Agent',
+    key: 'agent'
+  },
+  {
+    label: 'Language',
+    key: 'language'
+  },
+  {
+    label: '# TBs',
+    key: 'numberTalkingBooks'
+  }
+]
 
 export default {
   computed: {
@@ -88,18 +150,20 @@ export default {
   components: {
     VButton,
     Box,
-    ProgramSideMenu,
     ProgramRecipientsForm,
   },
   data: () => ({
     selectedRecipientIndex: 0,
-
-    showModal: false
+    cols,
+    showModal: {
+      edit: false,
+      mandatory: false
+    },
   }),
   mounted (){
     eventBus.$on('save-crud-data', () => {
       if (this.isFormFill) this.updateRecipients()
-      else this.onOpenModal()
+      else this.onOpenModal('mandatory', 'Required Fields')
     }),
     eventBus.$on('discard-crud-data', () => {
       this.fetchRecipients()
@@ -121,12 +185,17 @@ export default {
       'addRecipient',
       'removeRecipient',
     ]),
-    onOpenModal () {
-      this.showModal = true
-      this.setModal('Required Fields')
+    onClickEdit (index) {
+      this.selectedRecipientIndex = index
+      this.onOpenModal('edit', 'Recipient Details')
+    },
+    onOpenModal (modal, title) {
+      this.showModal[modal] = true
+      this.setModal(title)
     },
     onCloseModal () {
-      this.showModal = false
+      this.showModal.edit = false
+      this.showModal.mandatory = false
       this.closeModal()
     },
   }
