2fc2fcdb038d548fb5144eb241cc5c1b225fc0da
fnbellomo <fnbellomo@gmail.com>
2020-08-31 14:56:33 -0300

    Custom tooltip start

--- src/components/VTooltip.vue
+++ src/components/VTooltip.vue
@@ -7,9 +7,10 @@
     <slot />
 
     <span
-      :class="show ? 'opacity-100' : 'opacity-0'"
+      :class="[show ? 'opacity-100' : 'opacity-0', position === 'left' ? '-ml-4' : '']"
       class="absolute z-10 p-2 text-sm text-white text-center bg-gray-600 rounded-lg"
-      style="width: 200px; bottom: 100%; transition: opacity .6s; transform: translateX(-50%);"
+      style="width: 200px; bottom: 100%; transition: opacity .6s;"
+      :style="`transform: ${translate}`"
     >
       {{ text }}
     </span>
@@ -22,7 +23,23 @@ export default {
     text: {
       type: String,
       required: true
+    },
+    position: {
+      type: String,
+      default: 'center',
+      validator: (value) => {
+        return ['left', 'center', 'right'].indexOf(value) !== -1
+      }
     }
+  },
+  computed: {
+    translate () {
+      const amount = this.position === 'left' ? '0'
+        : this.position === 'center' ? '-50%'
+          : '-100%'
+
+      return `translateX(${amount});`
+    },
   },
   data: () => ({
     show: false
