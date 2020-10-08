c9ec1d6bd0185cc7635686ec00d782caca385922
fnbellomo <fnbellomo@gmail.com>
2020-10-08 17:28:28 -0300

    Fix missing paramenter

--- lambda/program_create.py
+++ lambda/program_create.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
             partner = event['partner'],
             affiliate = event['affiliate'],
         )
-        validate_user_access(program)
+        validate_user_access(event, program)
         deployments = program.default_deployments()
         contents = [Content(program_code=deplo.project, deployment=deplo.deployment)
             for deplo in deployments]
