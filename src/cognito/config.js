e522335b3b4c7a81499f28602018f23522d1fcd1
Matias Garcia Isaia <mgarcia@manas.tech>
2020-11-12 13:35:24 -0300

    Use production's Cognito user pool in production
    We're still sharing the one from staging for both development and staging

--- src/cognito/config.js
+++ src/cognito/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  UserPoolId: "us-west-2_05cdu7ShB",
-  ClientId: "7lkvnvm5c4i5mshov16jg67ori",
-  region: "us-west-2"
+  UserPoolId: process.env.VUE_APP_COGNITO_USER_POOL_ID || "us-west-2_05cdu7ShB",
+  ClientId: process.env.VUE_APP_COGNITO_CLIENT_ID || "7lkvnvm5c4i5mshov16jg67ori",
+  region: process.env.VUE_APP_COGNITO_REGION || "us-west-2"
 }
