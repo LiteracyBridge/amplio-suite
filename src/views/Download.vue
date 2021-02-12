38a8221f1d807a59f46c1b74c3b562dfdb9a6cfb
Bill Evans <billev2k@gmail.com>
2021-02-11 16:57:12 -0800

    Update Download.vue

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -7,8 +7,8 @@
     <section class="p-6 bg-white rounded-lg shadow-box">
       <div class="min-h-200-px py-5 text-left">
 
-        <p class="text-2xl font-semibold pt-5">
-          1. Download and install <a class="underline text-blue" href="/ACM_1.0_Installer.exe" download>ACM Bundle</a>. This installer includes:
+        <p class="text-2xl font-semibold pb-5">
+          1. Download and install <a class="underline text-blue" href="/AmplioSetup.exe" download>ACM Bundle</a>. This installer includes:
         </p>
 
         <ul class="pl-10 pt-5 text-2xl">
@@ -36,11 +36,11 @@
           2. Download and install <a class="underline text-blue" href="https://www.dropbox.com/downloading" target="_blank">Dropbox</a>
         </p>
 
-        <p class="text-2xl font-semibold pt-5">
+        <p class="text-2xl font-semibold pb-5">
           3. Download and install <a class="underline text-blue" href="https://www.audacityteam.org/download/" target="_blank">Audacity</a>
         </p>
 
-         <p class="text-2xl font-semibold pt-5">
+         <p class="text-2xl font-semibold pb-5">
           4. Download and install <a class="underline text-blue" href="https://www.tableau.com/products/reader" target="_blank">Tableau Reader</a>
         </p>
 
