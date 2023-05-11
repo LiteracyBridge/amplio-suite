79c2d8fb3e4c791525b5d5e859fb0d55dfc701b2
Bill Evans <billev2k@gmail.com>
2023-05-11 12:55:39 -0600

    Enable .../kb?article=Test-Article to link to a specific kb article.

--- frontend/src/views/kb.vue
+++ frontend/src/views/kb.vue
@@ -1,7 +1,25 @@
 <template>
     <main class="container mx-auto text-center">
         <section class="bg-white rounded-lg shadow-box">
-            <iframe src="https://amplio.my.site.com/help/s/" class="container mx-auto min-h-with-footer-header"></iframe>
+            <iframe :src="source"
+                    class="container mx-auto min-h-with-footer-header"></iframe>
         </section>
     </main>
 </template>
+
+<script>
+
+export default {
+    props: ['programId'],
+    computed: {
+        article() {
+            return this.$route.query.article || ''
+        },
+        source() {
+            let article = this.article ? `article/${this.article}` : ''
+            let src = "https://amplio.my.site.com/help/s/" + article;
+            return src;
+        },
+    },
+}
+</script>
