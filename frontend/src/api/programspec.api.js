0d5a8afc7985d1cfbbf825a0d97073d3b773ad42
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2023-10-09 19:07:17 +0000

    refactor(program-spec): add new api url

--- frontend/src/api/programspec.api.js
+++ frontend/src/api/programspec.api.js
@@ -1,25 +1,26 @@
-import store from '@/store'
+import store from "@/store";
 
-const URL2 = 'https://ng02lptr62.execute-api.us-west-2.amazonaws.com/Prod'
-const URL = 'https://v853rt57t9.execute-api.us-west-2.amazonaws.com/Prod'
-const PUBLISH = '/publish'
-const GET_CONTENT = '/get_content'
-const PUT_CONTENT = '/put_content'
-const DOWNLOAD = '/download'
+// const URL2 = 'https://ng02lptr62.execute-api.us-west-2.amazonaws.com/Prod'
+const URL2 = process.env.VUE_APP_API_URL;
+const URL = "https://v853rt57t9.execute-api.us-west-2.amazonaws.com/Prod";
+const PUBLISH = "/publish";
+// const GET_CONTENT = '/get_content'
+// const PUT_CONTENT = "/put_content";
+const DOWNLOAD = "/download";
 
 function makeInit(params) {
-    params = params || {}
-    const method = params.method || 'GET';
+    params = params || {};
+    const method = params.method || "GET";
     const data = params.data || undefined;
     const fileData = params.fileData || undefined;
     let init = {
         method: method,
-        cache: 'no-cache',
-        headers: {Authorization: store.state.account.user.token.jwtToken}
+        cache: "no-cache",
+        headers: { Authorization: store.state.account.user.token.jwtToken }
     };
     if (data) {
         init.body = JSON.stringify(data);
-        init.headers['Content-Type'] = 'application/json';
+        init.headers["Content-Type"] = "application/json";
     } else if (fileData) {
         init.body = fileData;
         //init.headers['Content-Type'] = 'application/text';
@@ -29,8 +30,11 @@ function makeInit(params) {
 
 async function publish(programid) {
     const init = makeInit();
-    const fetch_response = await fetch(`${URL}${PUBLISH}?programid=${programid}`, init);
-    return fetch_response.json()
+    const fetch_response = await fetch(
+        `${URL}${PUBLISH}?programid=${programid}`,
+        init
+    );
+    return fetch_response.json();
 }
 
 /**
@@ -40,32 +44,47 @@ async function publish(programid) {
  */
 async function getProgramSpec(programid) {
     const init = makeInit();
-    const fetch_response = await fetch(`${URL2}${GET_CONTENT}?programid=${programid}`, init);
+    const fetch_response = await fetch(
+        `${URL2}/program-spec/content?programid=${programid}`,
+        init
+    );
     return fetch_response.json();
 }
 
 async function putProgramSpec(programid, programspec) {
-    const init = makeInit({method: 'POST', data: programspec});
-    const fetch_response = await fetch(`${URL2}${PUT_CONTENT}?programid=${programid}`, init);
+    const init = makeInit({ method: "PUT", data: programspec });
+    const fetch_response = await fetch(
+        `${URL2}/program-spec/content?programid=${programid}`,
+        init
+    );
     return fetch_response.json();
 }
 
 async function getDownloadLink(programid, artifact) {
     const init = makeInit();
-    const fetch_response = await fetch(`${URL}${DOWNLOAD}?programid=${programid}&aslink=true&artifact=${artifact}`, init);
+    const fetch_response = await fetch(
+        `${URL}${DOWNLOAD}?programid=${programid}&aslink=true&artifact=${artifact}`,
+        init
+    );
     return fetch_response.json();
 }
 
 async function uploadSpec(programid, fileData) {
-    const init = makeInit({method: 'POST', fileData: fileData});
-    const fetch_request = new Request(`${URL}/upload?programid=${programid}&return_diff=t`, init);
+    const init = makeInit({ method: "POST", fileData: fileData });
+    const fetch_request = new Request(
+        `${URL}/upload?programid=${programid}&return_diff=t`,
+        init
+    );
     const fetch_response = await fetch(fetch_request);
     return fetch_response.json();
 }
 
 async function approveSpec(programid, publish) {
-    const init = makeInit({method: 'GET'});
-    const fetch_request = new Request(`${URL}/accept?programid=${programid}&publish=${publish ? 't' : 'f'}`, init);
+    const init = makeInit({ method: "GET" });
+    const fetch_request = new Request(
+        `${URL}/accept?programid=${programid}&publish=${publish ? "t" : "f"}`,
+        init
+    );
     const fetch_response = await fetch(fetch_request);
     return fetch_response.json();
 }
@@ -74,11 +93,10 @@ export {
     publish,
     getProgramSpec,
     putProgramSpec,
-
     // getContent,
     // getContent2,
     // putContent,
     getDownloadLink,
     uploadSpec,
-    approveSpec,
-}
+    approveSpec
+};
