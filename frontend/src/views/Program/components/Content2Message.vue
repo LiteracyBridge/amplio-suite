ef8814df6d8251496be16dd51b51537689a39cd8
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-01-31 13:31:37 +0000

    refactor(spec): cleanup message ui

---
+++ frontend/src/views/Program/components/Content2Message.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="border rounded my-1">
+    <Alert
+      v-if="duplicateTitles.includes(message.title)"
+      type="warning"
+      show-icon
+      class="my-2"
+      message="Duplicate message title in this playlist"
+    />
+
+    <Row :gutter="8">
+      <Col :span="2">
+        <div class="flex">
+          <div class="m-2 p-2 cursor-grab msg-handle">
+            <font-awesome-icon icon="grip-lines" />
+          </div>
+
+          <div class="m-2 py-2 mt-2" style="min-width: 10px" @click="toggleExpanded">
+            <font-awesome-icon :icon="icon" size="lg" />
+          </div>
+        </div>
+      </Col>
+
+      <Col :span="14">
+        <FormItem
+          help='Message title cannot contain these characters: \/:*?\<>|"'
+          class="mt-3 w-full"
+        >
+          <Input
+            aria-label="`message ${message.title}`"
+            placeholder="Message Title"
+            type="text"
+            :name="`message-${message.title}`"
+            :value="message.title"
+            @input="
+              store.setMessageTitle({
+                deployment,
+                playlist,
+                message,
+                title: $event.target.value,
+              })
+            "
+          /> </FormItem
+      ></Col>
+      <Col :span="4" align="center">
+        <Popconfirm
+          title="Are you sure to delete this message?"
+          ok-text="Yes"
+          cancel-text="No"
+          @click="queryDeleteMessage()"
+        >
+          <Button class="mt-3" :aria-label="`Delete message ${message.title}`" :danger="true"
+            >Delete Message</Button
+          >
+        </Popconfirm>
+      </Col>
+    </Row>
+
+    <!--   <div class="flex"> -->
+    <!-- <div class="m-2 p-2 cursor-grab msg-handle">
+        <font-awesome-icon icon="grip-lines" />
+      </div>
+
+      <div class="m-2 py-2 mt-2" style="min-width: 10px" @click="toggleExpanded">
+        <font-awesome-icon :icon="icon" size="lg" />
+      </div> -->
+
+    <!-- <v-tooltip
+      v-if="duplicateTitles.includes(message.title)"
+      text="Duplicate message title in this playlist"
+      class="my-auto border-none"
+    >
+      <font-awesome-icon class="text-orange-600 ml-2 my-auto" icon="exclamation-circle" />
+    </v-tooltip>
+
+    <VButton
+      class="mt-4 ml-2 border-none"
+      iconL="trash-alt"
+      variant="warning"
+      :ariaLabel="`Delete message ${message.title}`"
+      @click="queryDeleteMessage()"
+    /> -->
+    <!-- </div> -->
+
+    <!-- Form for editing the details of a message -->
+    <div class="pl-10">
+      <div
+        :class="expanded ? 'h-104 md:h-96' : 'h-0'"
+        class="transition-all duration-300"
+      >
+        <content2-message-form
+          v-if="expanded"
+          :deployment="deployment"
+          :playlist="playlist"
+          :message="message"
+        />
+      </div>
+    </div>
+
+    <!-- For delete confirmation modal components -->
+    <!-- <portal to="modalBody" v-if="modal.show">
+      <p class="my-5">Are you sure you want to delete the message?</p>
+    </portal>
+
+    <portal to="modalFooter" v-if="modal.show">
+      <footer class="flex flex-row-reverse justify-between">
+        <VButton label="Delete Message" variant="warning" @click="confirmDeleteMessage" />
+        <VButton label="Do Not Delete" @click="cancelDeleteMessage" />
+      </footer>
+    </portal> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import Content2MessageForm from "@/components/Content2MessageForm.vue";
+import { useProgramSpecStore } from "@/store/programspec";
+import { useUIStore } from "@/store/ui";
+import { computed, onMounted, ref } from "vue";
+import { Playlist } from "@/models/playlist";
+import { Message } from "@/models/message";
+import { Deployment } from "@/models/deployment";
+import { FormItem, Input, Row, Col, Alert, Button, Popconfirm } from "ant-design-vue";
+
+const props = defineProps<{
+  deployment: Deployment;
+  playlist: Playlist;
+  message: Message;
+  duplicateTitles: string[];
+}>();
+
+const store = useProgramSpecStore();
+
+const expanded = ref(false),
+  modal = ref({
+    show: false,
+    eleIndex: -1,
+  });
+
+const icon = computed(() => {
+  return expanded.value ? "caret-down" : "caret-right";
+});
+
+onMounted(() => {
+  if (props.message.title.length === 0) {
+    expanded.value = true;
+  }
+});
+
+function toggleExpanded() {
+  expanded.value = !expanded.value;
+}
+
+function queryDeleteMessage() {
+  modal.value.show = true;
+  useUIStore().setModal("Delete Message");
+}
+
+function cancelDeleteMessage() {
+  modal.value.show = false;
+  useUIStore().closeModal();
+}
+
+function confirmDeleteMessage() {
+  store.removeMessage({
+    deployment: props.deployment,
+    playlist: props.playlist,
+    message: props.message,
+  });
+  cancelDeleteMessage();
+}
+</script>
--- frontend/src/views/Program/components/Content2Playlist.vue
+++ frontend/src/views/Program/components/Content2Playlist.vue
@@ -38,79 +38,41 @@
           :aria-label="`Delete playlist ${playlist.title}`"
           :danger="true"
           class="ml-3"
-        >Delete Playlist</Button>
+          >Delete Playlist</Button
+        >
       </Popconfirm>
     </Col>
   </Row>
 
-  <div class="flex mb-2">
-    <div class="cursor-grab pl-handle ml-10 mr-2">
-      <font-awesome-icon icon="grip-lines" />
-    </div>
-    <div class="mx-2" style="min-width: 10px" @click="onToggleExpanded">
-      <font-awesome-icon :icon="icon" size="lg" />
-    </div>
-    <div class="w-full tag-playlist">
-      <div class="flex font-light text-gray-800">
-        <span class="flex-initial text-xl">Playlist {{ playlist.position }}:</span>
-        <!-- -mt-1 to push the input up just a bit, to better align with the label -->
-        <!-- <input
-          class="flex-auto mx-2 px-1 -mt-1 border rounded-sm text-xl text-black"
-          :class="
-            !title || title.length === 0 ? 'invalid border-red-500 border-2 rounded' : ''
-          "
-          v-model="title"
-          placeholder="Playlist Title"
-        /> -->
-        <!-- <button
-          v-if="expanded"
-          class="btn text-base"
-          @click="onAddMessage()"
-          :disabled="!canAddMessage"
-        >
-          Add Message
-        </button> -->
-
-        <!-- <VButton
-          v-if="canRemovePlaylist"
-          class="flex-initial my-auto ml-2 border-none"
-          iconL="trash-alt"
-          variant="warning"
-          :ariaLabel="`Delete playlist ${title}`"
-          @click="onRemovePlaylist()"
-        /> -->
-      </div>
-
-      <div class="tag-message-container" v-if="expanded">
-        <draggable
-          v-model="messages"
-          :animation="200"
-          handle=".msg-handle"
-          group="message"
-          ghost-class="moving-item"
-          @start="dragging = true"
-          @end="dragging = false"
-          item-key="position"
-        >
-          <template #item="{ element: message, index: index }">
-            <div :key="index" class="flex tag-message">
-              <content2-message
-                class="w-full"
-                :deployment="deployment"
-                :playlist="playlist"
-                :message="message"
-                :duplicateTitles="duplicateTitles"
-              ></content2-message>
-            </div>
-          </template>
-        </draggable>
-      </div>
+  <div class="my-4 ml-20 w-full">
+    <div v-if="expanded">
+      <draggable
+        v-model="messages"
+        :animation="200"
+        handle=".msg-handle"
+        group="message"
+        ghost-class="moving-item"
+        @start="dragging = true"
+        @end="dragging = false"
+        item-key="position"
+      >
+        <template #item="{ element: message, index: index }">
+          <div :key="index">
+            <content2-message
+              :deployment="deployment"
+              :playlist="playlist"
+              :message="message"
+              :duplicateTitles="duplicateTitles"
+            ></content2-message>
+          </div>
+        </template>
+      </draggable>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Content2Message from "@/components/Content2Message.vue";
+import Content2Message from "./Content2Message.vue";
 import Draggable from "vuedraggable";
 import { Row, Col, Input, Button, Popconfirm } from "ant-design-vue";
 import { useProgramSpecStore } from "@/store/programspec";
@@ -126,27 +88,6 @@ const props = defineProps<{
 const store = useProgramSpecStore();
 const expanded = ref(false),
   dragging = ref(false);
-// export default {
-//   props: {
-//     deployment: {
-//       type: Object,
-//       required: true,
-//     },
-
-//     playlist: {
-//       type: Object,
-//       required: true,
-//     },
-//   },
-
-//   components: {
-//     Content2Message,
-//     Draggable,
-//     VButton,
-//   },
-
-// computed: {
-//   ...mapState(useProgramSpecStore, ["deployments"]),
 
 const canAddMessage = computed(() => {
   return (
@@ -172,33 +113,6 @@ const icon = computed(() => {
   return expanded.value ? "caret-down" : "caret-right";
 });
 
-// const title = computed({
-//   get() {
-//     return props.playlist.title;
-//   },
-//   set(newValue) {
-//     store.setPlaylistTitle({
-//       deployment: this.deployment,
-//       playlist: this.playlist,
-//       title: newValue,
-//     });
-//   },
-// });
-
-// const audience = computed({
-//   get() {
-//     if (!props.playlist.audience || props.playlist.audience === "null") return "";
-//     return props.playlist.audience;
-//   },
-//   set(newValue) {
-//     store.setPlaylistAudience({
-//       deployment: this.deployment,
-//       playlist: this.playlist,
-//       audience: newValue,
-//     });
-//   },
-// });
-
 const messages = computed({
   get() {
     return props.playlist.messages;
@@ -212,20 +126,6 @@ const messages = computed({
   },
 });
 
-// data() {
-//   return {
-//     expanded: false,
-//   };
-// },
-
-// methods: {
-//   ...mapActions(useProgramSpecStore, [
-//     "removePlaylist",
-//     "setPlaylistTitle",
-//     "setPlaylistAudience",
-//     "setMessages",
-//     "addMessage",
-//   ]),
 function onToggleExpanded() {
   expanded.value = !expanded.value;
 }
@@ -242,5 +142,3 @@ function onRemovePlaylist() {
   }
 }
 </script>
-
-<style scoped></style>
