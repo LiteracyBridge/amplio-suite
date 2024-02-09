f1280e10de7661158c69ef2255d0a932e6408a33
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-02-09 10:52:28 +0000

    feat(uf): add search to message browser table

--- frontend/src/views/UserFeedback/Analysis/MessagesBrowser.vue
+++ frontend/src/views/UserFeedback/Analysis/MessagesBrowser.vue
@@ -1,68 +1,86 @@
 <script setup lang="ts">
-import {
-  Card,
-  Button,
-  Form,
-  FormItem,
-  Space,
-  Divider,
-  Textarea,
-  Table,
-  TabPane,
-  CheckboxGroup,
-  RadioGroup,
-  Checkbox,
-  Radio,
-  Empty,
-  notification,
-  PageHeader,
-  Alert,
-  Spin,
-} from "ant-design-vue";
-import { computed, h, onMounted, ref, watch } from "vue";
+import { Button, Table, Alert, Input } from "ant-design-vue";
+import type { TableColumnsType } from "ant-design-vue";
+import { computed, h, onMounted, reactive, ref, watch } from "vue";
 import { useFeedbackAnalysis } from "@/store/feedback_analysis.store";
 import { useAppStore } from "@/store/app.store";
 import { UserFeedbackMessage } from "@/models/uf_message";
+import { ReloadOutlined, SearchOutlined } from "@ant-design/icons-vue";
 
 const feedbackStore = useFeedbackAnalysis(),
   store = useAppStore();
 
-const columns = [
+const messages = ref<UserFeedbackMessage[]>([]);
+const groupSearchInput = ref();
+const state = reactive({
+  searchText: "",
+  searchedColumn: "",
+});
+
+const columns = ref<TableColumnsType>([
   {
     key: "actions",
+    fixed: "left",
+    width: 250,
   },
   {
     title: "ID",
     key: "uuid",
+    width: 150,
+    fixed: "left",
   },
   {
     title: "Message Title",
     key: "title",
+    resizable: true,
+    width: 150,
   },
   {
     title: "Feedback",
     key: "feedback",
+    width: 80,
   },
   {
     title: "Group",
     key: "group",
-  },
-  {
-    title: "Community",
-    key: "community",
+    width: 150,
+    customFilterDropdown: true,
+    onFilter: (value: any, record: UserFeedbackMessage) =>
+      record?.recipient?.group_name
+        ?.toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => {
+          groupSearchInput.value.focus();
+        }, 100);
+      }
+    },
   },
   {
     title: "Location",
     key: "location",
+    width: 170,
   },
   {
     title: "Transcription",
     key: "transcription",
-    elipsis: true,
+    ellipsis: true,
+    width: 230,
   },
-];
+]);
 
-const messages = ref<UserFeedbackMessage[]>([]);
+const handleSearch = (selectedKeys: any, confirm: any, dataIndex: any) => {
+  confirm();
+  state.searchText = selectedKeys[0];
+  state.searchedColumn = dataIndex;
+};
+
+const handleReset = (clearFilters: any) => {
+  clearFilters({ confirm: true });
+  state.searchText = "";
+};
 
 async function fetchMessages() {
   await feedbackStore.fetchSampleMessages().then((resp) => (messages.value = resp));
@@ -79,12 +97,49 @@ onMounted(() => {
     :data-source="messages"
     :loading="feedbackStore.loading"
     size="small"
+    :scroll="{ x: 1500 }"
   >
     <template #title>
       <div class="flex justify-between">
         <span></span>
         <Button type="primary" @click="fetchMessages()" :ghost="true">
-          <ReloadOutlined /> Refresh Data
+          <template #icon>
+            <ReloadOutlined />
+          </template>
+          Refresh Data
+        </Button>
+      </div>
+    </template>
+
+    <template
+      #customFilterDropdown="{
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+        column,
+      }"
+    >
+      <div style="padding: 8px">
+        <Input
+          ref="groupSearchInput"
+          :placeholder="`Search ${column.dataIndex}`"
+          :value="selectedKeys[0]"
+          style="width: 188px; margin-bottom: 8px; display: block"
+          @change="(e: any) => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+          @pressEnter="handleSearch(selectedKeys, confirm, column.dataIndex)"
+        />
+        <Button
+          type="primary"
+          size="small"
+          style="width: 90px; margin-right: 8px"
+          @click="handleSearch(selectedKeys, confirm, column.dataIndex)"
+        >
+          <template #icon><SearchOutlined /></template>
+          Search
+        </Button>
+        <Button size="small" style="width: 90px" @click="handleReset(clearFilters)">
+          Reset
         </Button>
       </div>
     </template>
@@ -92,7 +147,6 @@ onMounted(() => {
     <template #bodyCell="{ column, record }">
       <template v-if="column.key === 'actions'">
         <audio controls :src="record.url" preload="none"></audio>
-        <!-- <audio id="audio1" src="http://www.guidetojapanese.org/audio/wa.mp3"></audio> -->
       </template>
 
       <template v-if="column.key === 'title'">
@@ -112,7 +166,8 @@ onMounted(() => {
       </template>
 
       <template v-if="column.key === 'location'">
-        {{ record.recipient?.district }}, {{ record.recipient?.community_name }}, {{ record.recipient?.region }}
+        {{ record.recipient?.district }}, {{ record.recipient?.community_name }},
+        {{ record.recipient?.region }}
       </template>
 
       <template v-if="column.key === 'transcription'">
