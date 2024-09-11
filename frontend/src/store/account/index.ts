0c5e114f35452462261164decf4cfafd83371947
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-09-11 08:16:49 +0000

    fix: permission check bug

--- frontend/src/store/account/index.ts
+++ frontend/src/store/account/index.ts
@@ -90,6 +90,10 @@ export const useAccountStore = defineStore("account", {
         return action.some(p => this.can(p));
       }
 
+      if(this.$state.user.permissions == null) {
+        return false
+      }
+
       if(this.$state.user.permissions[action] == null) {
         return false
       }
