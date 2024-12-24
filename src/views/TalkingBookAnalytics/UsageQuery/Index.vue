b33d364142e16be3b4fbdc8e6fa90fb68d502e1e
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-12-24 09:40:36 +0000

    fix(usage-query): update query builder

--- src/views/TalkingBookAnalytics/UsageQuery/Index.vue
+++ src/views/TalkingBookAnalytics/UsageQuery/Index.vue
@@ -1,25 +1,20 @@
 <script setup lang="ts">
 import { useTalkingBookAnalyticStore } from "@/store/tb_analytics.store";
 import {
-  Tag,
-  Row,
-  Tooltip,
   Table,
   PageHeader,
-  Divider,
   Button,
   Dropdown,
   MenuItem,
-  Alert,
   Menu,
-  Modal,
   notification,
 } from "ant-design-vue";
 import { onMounted, ref } from "vue";
 import { useAppStore } from "@/store/app.store";
-import type { Deployment } from "@/models/deployment";
 import { DownOutlined } from "@ant-design/icons-vue";
 import QueryBuilder from "./QueryBuilder.vue";
+import { Workbook } from "exceljs";
+import { DateTime } from "luxon";
 
 const store = useTalkingBookAnalyticStore();
 const appStore = useAppStore();
@@ -96,15 +91,45 @@ async function fetchStats(q: string, group: string) {
   rows.value = results;
 }
 
-// onMounted(async () => {
-//   if (selectedDeployment.value == null) {
-//     const count = appStore.deployments.length;
-//     if (count > 1) {
-//       // await fetchStats(appStore.deployments[count - 1]);
-//       // await fetchStats(appStore.deployments[count - 1]);
-//     }
-//   }
-// });
+async function exportReport() {
+  notification.info({
+    message: "Downloading Usage Query",
+  });
+
+  const workbook = new Workbook();
+  const sheet = workbook.addWorksheet("Report");
+  sheet.columns = Object.keys(rows.value[0]).map((k) => ({
+    header: k,
+    key: k,
+  }));
+  sheet.addRows(rows.value);
+
+  const buffer = await workbook.xlsx.writeBuffer();
+  const blob = new Blob([buffer], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `usage_query_report_${DateTime.now()
+    .toLocaleString(DateTime.DATE_FULL)
+    .replace(",", "_")}.xlsx`;
+  a.click();
+  window.URL.revokeObjectURL(url);
+
+  notification.success({
+    description: "Usage query report exported successfully.",
+    message: "Success",
+  });
+}
+
+onMounted(async () => {
+  const rpt = reports.find((r) => r.key === "msg");
+
+  selectedDeployment.value = "all";
+  query.value = rpt.query;
+  fetchStats(rpt.query, rpt.group);
+});
 </script>
 
 <template>
@@ -126,7 +151,9 @@ async function fetchStats(q: string, group: string) {
           </Menu>
         </template>
         <Button>
-          Change Deployment
+          <span v-if="selectedDeployment == null">Choose Deployment</span>
+          <span v-if="selectedDeployment == 'all'">All Deployments</span>
+          <span v-else>Deployment {{ selectedDeployment }}</span>
           <DownOutlined />
         </Button>
       </Dropdown>
@@ -153,7 +180,8 @@ async function fetchStats(q: string, group: string) {
           </Menu>
         </template>
         <Button>
-          Choose Report
+          <span v-if="query == null">Choose Report</span>
+          <span v-else>{{ reports.find((r) => r.query == query).title }}</span>
           <DownOutlined />
         </Button>
       </Dropdown>
@@ -180,6 +208,11 @@ async function fetchStats(q: string, group: string) {
     :row-class-name="(_record, index) => (index % 2 === 1 ? 'table-striped' : null)"
     class="ant-table-striped"
   >
+    <template #title>
+      <Button :disabled="rows.length === 0" @click="exportReport()" ghost type="primary"
+        >Download Report</Button
+      >
+    </template>
   </Table>
 
   <div v-if="modalVisible">
@@ -190,187 +223,3 @@ async function fetchStats(q: string, group: string) {
     />
   </div>
 </template>
-
-<style scoped>
-/* .ant-table-striped :deep(.table-striped) td {
-  background-color: #fafafa;
-} */
-.ant-table-striped :deep(.table-striped) td {
-  background-color: #fafafa;
-}
-/* Incredibly over-specified styles for query builder. */
-
-/*!
- * jQuery QueryBuilder 2.5.2
- * Copyright 2014-2018 Damien "Mistic" Sorel (http://www.strangeplanet.fr)
- * Licensed under MIT (https://opensource.org/licenses/MIT)
- */
-.query-builder .rule-container,
-.query-builder .rule-placeholder,
-.query-builder .rules-group-container {
-  position: relative;
-  margin: 4px 0;
-  border-radius: 5px;
-  padding: 5px;
-  border: 1px solid #eee;
-  background: rgba(255, 255, 255, 0.9);
-}
-
-/*.query-builder .error-container,*/
-.query-builder .rule-container .rule-filter-container,
-.query-builder .rule-container .rule-operator-container,
-.query-builder .rule-container .rule-value-container {
-  display: inline-block;
-  margin: 0 5px 0 0;
-  vertical-align: middle;
-}
-
-.query-builder .rules-group-container {
-  padding: 10px 10px 6px;
-  border: 1px solid #dcc896;
-  background: rgba(250, 240, 210, 0.5);
-}
-
-.query-builder .rules-group-header {
-  margin-bottom: 10px;
-}
-
-.query-builder .rules-group-header .group-conditions .btn.readonly:not(.active),
-.query-builder .rules-group-header .group-conditions input[name$="_cond"] {
-  border: 0;
-  clip: rect(0 0 0 0);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  width: 1px;
-  white-space: nowrap;
-}
-
-.query-builder .rules-group-header .group-conditions .btn.readonly {
-  border-radius: 3px;
-}
-
-.query-builder .rules-list {
-  list-style: none;
-  padding: 0 0 0 15px;
-  margin: 0;
-}
-
-/*.query-builder .rule-value-container {*/
-/*    border-left: 1px solid #ddd;*/
-/*    padding-left: 5px*/
-/*}*/
-
-/*.query-builder .rule-value-container label {*/
-/*    margin-bottom: 0;*/
-/*    font-weight: 400*/
-/*}*/
-
-/*.query-builder .rule-value-container label.block {*/
-/*    display: block*/
-/*}*/
-
-/*.query-builder .rule-value-container input[type=number], .query-builder .rule-value-container input[type=text],*/
-/*.query-builder .rule-value-container select {*/
-/*    padding: 1px*/
-/*}*/
-
-/*.query-builder .error-container {*/
-/*    display: none;*/
-/*    cursor: help;*/
-/*    color: red*/
-/*}*/
-
-.query-builder .has-error {
-  background-color: #fdd;
-  border-color: #f99;
-}
-
-/*.query-builder .has-error .error-container {*/
-/*    display: inline-block !important*/
-/*}*/
-
-.query-builder .rules-list > ::after,
-.query-builder .rules-list > ::before {
-  content: "";
-  position: absolute;
-  left: -10px;
-  width: 10px;
-  height: calc(50% + 4px);
-  border-color: #ccc;
-  border-style: solid;
-}
-
-.query-builder .rules-list > ::before {
-  top: -4px;
-  border-width: 0 0 2px 2px;
-}
-
-.query-builder .rules-list > ::after {
-  top: 50%;
-  border-width: 0 0 0 2px;
-}
-
-.query-builder .rules-list > :first-child::before {
-  top: -12px;
-  height: calc(50% + 14px);
-}
-
-.query-builder .rules-list > :last-child::before {
-  border-radius: 0 0 0 4px;
-}
-
-.query-builder .rules-list > :last-child::after {
-  display: none;
-}
-
-.query-builder.bt-checkbox-glyphicons
-  .checkbox
-  input[type="checkbox"]:checked
-  + label::after {
-  font-family: "Glyphicons Halflings";
-  content: "\e013";
-}
-
-.query-builder.bt-checkbox-glyphicons .checkbox label::after {
-  padding-left: 4px;
-  padding-top: 2px;
-  font-size: 9px;
-}
-
-/*.query-builder .error-container + .tooltip .tooltip-inner {*/
-/*    color: #f99 !important*/
-/*}*/
-
-/*.query-builder p.filter-description {*/
-/*    margin: 5px 0 0 0;*/
-/*    background: #d9edf7;*/
-/*    border: 1px solid #bce8f1;*/
-/*    color: #31708f;*/
-/*    border-radius: 5px;*/
-/*    padding: 2.5px 5px;*/
-/*    font-size: .8em*/
-/*}*/
-
-.query-builder .rules-group-header [data-invert] {
-  margin-left: 5px;
-}
-
-.query-builder .dragging {
-  position: fixed;
-  opacity: 0.5;
-  z-index: 100;
-}
-
-.query-builder .dragging::after,
-.query-builder .dragging::before {
-  display: none;
-}
-
-.query-builder .rule-placeholder {
-  border: 1px dashed #bbb;
-  opacity: 0.7;
-}
-</style>
