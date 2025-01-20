32025a0a87a62d753f3eaeea28a71dbdc81ad3ea
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-01-20 10:48:42 +0000

    fix: env var naming inconsistency

--- src/main.ts
+++ src/main.ts
@@ -94,8 +94,8 @@ if (import.meta.env.DEV) {
   Amplify.configure({
     ...awsconfig,
     aws_user_pools_web_client_id: import.meta.env
-      .VITE_APP_AWS_USER_POOLS_CLIENT_ID || awsconfig.aws_user_pools_web_client_id,
-    aws_user_pools_id: import.meta.env.VITE_APP_AWS_USER_POOLS_ID || awsconfig.aws_user_pools_id,
+      .VITE_APP_COGNITO_CLIENT_ID || awsconfig.aws_user_pools_web_client_id,
+    aws_user_pools_id: import.meta.env.VITE_APP_COGNITO_USER_POOL_ID || awsconfig.aws_user_pools_id,
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   } as any);
 } else {
