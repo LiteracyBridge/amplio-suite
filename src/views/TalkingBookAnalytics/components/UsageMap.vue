fd2497882f812bdf4bff36cead24746ee51c7799
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-01-28 09:10:12 +0000

    chore(map): house cleaning

--- src/views/TalkingBookAnalytics/components/UsageMap.vue
+++ src/views/TalkingBookAnalytics/components/UsageMap.vue
@@ -13,12 +13,13 @@ import TileLayer from "ol/layer/Tile.js";
 import View from "ol/View.js";
 import { fromLonLat } from "ol/proj.js";
 import "ol/ol.css";
-import { onMounted, ref, type VNodeRef } from "vue";
+import { onMounted, ref } from "vue";
+import type { VNodeRef } from "vue";
 import type { Recipient } from "@/models/recipient";
 import TalkingBook from "@/assets/images/talking-book.png";
 
 const props = defineProps<{
-  data: Array<Recipient & {played_minutes: number, played_seconds: number}>;
+  data: Array<Recipient & { played_minutes: number; played_seconds: number }>;
   centroid: { latitude: number; longitude: number };
 }>();
 const popup = ref<VNodeRef>();
@@ -35,7 +36,6 @@ onMounted(() => {
       center: fromLonLat([props.centroid.longitude, props.centroid.latitude]),
       zoom: 7,
     }),
-    // sources: []
   });
 
   // Add recipients location as markers to the map
@@ -64,8 +64,8 @@ onMounted(() => {
   });
   map.addLayer(layer);
 
-  const container = document.getElementById('ol-popup');
-  const closer = document.getElementById('ol-popup-closer');
+  const container = document.getElementById("ol-popup");
+  const closer = document.getElementById("ol-popup-closer");
   const overlay = new Overlay({
     element: container,
     autoPan: true,
@@ -75,17 +75,34 @@ onMounted(() => {
     // },
   });
   map.addOverlay(overlay);
+
   map.on("singleclick", (event) => {
-    container.style.visibility = "visible"
+    container.style.visibility = "visible";
     if (map.hasFeatureAtPixel(event.pixel) === true) {
       console.log(event);
       const coordinate = event.coordinate;
 
-      const recipient = props.data.find(r => r.id == map.getFeaturesAtPixel(event.pixel)[0].getId())!
+      const recipient = props.data.find(
+        (r) => r.id == map.getFeaturesAtPixel(event.pixel)[0].getId()
+      )!;
 
-      console.log(map.getFeaturesAtPixel(event.pixel))
+      console.log(map.getFeaturesAtPixel(event.pixel));
       // @ts-ignore
-      document.getElementById("ol-popup-content").innerHTML = `<strong>District: </strong> ${recipient.district || 'N/A'} <br/> <strong>Group: </strong> ${recipient.group || 'N/A'}<br /> <strong>Community: </strong> ${recipient.community_name || 'N/A'}<br /> <strong>Talking Books Assigned: </strong>  ${recipient.numtbs || 0} <br/> <strong>Time Played (in seconds): </strong> ${recipient.played_seconds || 0} <br/> <strong>Time Played (in minutes): </strong> ${recipient.played_minutes || 0} <br/> `;
+      document.getElementById(
+        "ol-popup-content"
+      ).innerHTML = `<strong>District: </strong> ${
+        recipient.district || "N/A"
+      } <br/> <strong>Group: </strong> ${
+        recipient.group || "N/A"
+      }<br /> <strong>Community: </strong> ${
+        recipient.community_name || "N/A"
+      }<br /> <strong>Talking Books Assigned: </strong>  ${
+        recipient.numtbs || 0
+      } <br/> <strong>Time Played (in seconds): </strong> ${
+        recipient.played_seconds || 0
+      } <br/> <strong>Time Played (in minutes): </strong> ${
+        recipient.played_minutes || 0
+      } <br/> `;
       overlay.setPosition(coordinate);
     } else {
       overlay.setPosition(undefined);
@@ -105,23 +122,10 @@ onMounted(() => {
       href="#"
       id="ol-popup-closer"
       class="ol-popup-closer text-black float-end text-lg block mx-2"
-      :onclick="($el) =>{$el.target.parentElement.style.visibility = 'hidden';}"
+      :onclick="($el:any) =>{$el.target.parentElement.style.visibility = 'hidden';}"
       >x</a
     >
     <div id="ol-popup-content" class="p-2"></div>
-    <!-- <div class=" w-1/3">
-      <div class="">
-        <h3 class="text-lg font-semibold">Modal Title</h3>
-        <button class="text-black close-modal" onclick="closeModal()">&times;</button>
-      </div>
-      <div >
-        <p>This is a sample popup modal using Tailwind CSS.</p>
-      </div>
-      <div class="border-t px-4 py-2 flex justify-end">
-        <button class="bg-gray-500 text-white px-4 py-2 rounded mr-2 close-modal" onclick="closeModal()">Close</button>
-        <button class="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
-      </div>
-    </div> -->
   </div>
 </template>
 
