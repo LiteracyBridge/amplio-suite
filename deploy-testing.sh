aa411acc58fe6acbf418a69c5dbe877e35f0104c
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-04-07 10:50:20 +0000

    ci: update package script

--- deploy-testing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set pipefail -euo
-
-docker build --tag test-suite --build-arg PORT=6001 .
-    docker run --publish 127.0.0.1:6001:6001 \
-        --restart always \
-        --detach test-suite
